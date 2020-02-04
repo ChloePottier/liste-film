b0a8fdec76cfe107115d9e902f478cc76a9e6619
ChloePottier <chloe.pottier@free.fr>
2020-02-04 16:14:25 +0100

    location: user, film, durée location, date début location

--- location.php
+++ location.php
@@ -33,41 +33,47 @@
         //resultat de la requete
         $reqFilm = $dbh->query($sqlFilm);
      
-        // if(!empty($_POST['id_client'])AND !empty($_POST['id_film'])){
-    
-        $idClient = $_POST['id_client'];
-        $idFilm = $_POST['id_film'];
+        if(!empty($_GET['id_client'])AND !empty($_GET['id_film']) AND !empty($_GET['dureeLoc']) AND !empty($_GET['dateLoc'])){
+            $idFilm = $_GET['id_film'];
+            $idClient = $_GET['id_client'];
+            $dureeLoc = $_GET['dureeLoc'];
+            $dateLoc = $_GET['dateLoc'];
 
-
-        // $tab = array(
-        //     'id_film' => $idFilm,
-        //     'id_client'=>$idClient);
-        //     //la requête doit comporter id et titre sinon ca ne marche pas
-        //     $sqlAjout ="INSERT INTO location VALUES(:id_film, :id_client)";
-        //     $reqAjout = $dbh->prepare($sqlAjout);
-        //     $reqAjout ->execute($tab);
-        //     if($reqAjout == true){
-        //         echo "Location enregistré";
-        //     } else {
-        //         echo "Impossible d'enregistrer la location";
-        //     }
+            echo $idFilm ." ".$idClient." ".$dureeLoc." ".$dateLoc;
+            $tab = array(
+                'id_film' => $idFilm,
+                'id_client'=>$idClient,
+                'Duree' => $dureeLoc,
+                'date_debut_location' => $dateLoc
+                );
+            // la requête doit comporter id et titre sinon ca ne marche pas
+            $sqlLoc ="INSERT INTO location VALUES(:id_film, :id_client, :Duree, :date_debut_location)";
+            $reqLoc = $dbh->prepare($sqlLoc);
+            $reqLoc ->execute($tab);
+            if($reqLoc == true){
+                echo "Location enregistré";
+            } else {
+                echo "Impossible d'enregistrer la location";
+            };
+        };
     ?>
 
-<form method="get" action="#">	
+    <form method="get" action="#">	
     <select name="id_client">
-        <?php
-        while($reqUserFinal=$reqUser->fetch(PDO::FETCH_ASSOC)){
-            echo '<option value="'. $reqUserFinal['id'].'" selected>'.$reqUserFinal['Nom']." " .$reqUserFinal['Prenom'].'</option> ';
-        }
-        ?>
-    </select>
-	<select name="id_film">
-    <!-- tant que c'est possible afficher la liste des films -->
-        <?php while($reqFilmFinal=$reqFilm->fetch(PDO::FETCH_ASSOC)){
-            echo '<option value="'. $reqFilmFinal['id'].'" selected>'.$reqFilmFinal["titre"].'</option> ';
-        }?>
-	</select>
-	<input type="submit" value="Louer" />
-</form>
+            <?php
+            while($reqUserFinal=$reqUser->fetch(PDO::FETCH_ASSOC)){
+                echo '<option value="'. $reqUserFinal['id'].'" selected>'.$reqUserFinal['Nom']." " .$reqUserFinal['Prenom'].'</option> ';
+            }
+            ?>
+        </select>
+        <select name="id_film">
+            <?php while($reqFilmFinal=$reqFilm->fetch(PDO::FETCH_ASSOC)){
+                echo '<option value="'. $reqFilmFinal['id'].'" selected>'.$reqFilmFinal["titre"].'</option> ';
+            }?>
+        </select>
+        <input style = 'width: 300px;' name='dureeLoc' type='number' /> 
+        <input type="date" name="dateLoc">
+        <button type="submit" value="Louer">Louer</button>
+    </form>
 </body>
 </html>
